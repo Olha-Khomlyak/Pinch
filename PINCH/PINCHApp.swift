7ecf0953199b66d48d015c869d7f7a81b322b6ec
Olha Khomlyak <olga.khomlyak@gmail.com>
2022-12-03 16:11:35 +0300

    Initial Commit

---
+++ PINCH/PINCHApp.swift
@@ -0,0 +1,17 @@
+//
+//  PINCHApp.swift
+//  PINCH
+//
+//  Created by Olha Khomlyak on 3.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PINCHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
